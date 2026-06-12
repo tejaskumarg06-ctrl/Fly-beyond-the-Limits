032d99740a8317043ff380b61e020260e3c0f3c2
tejas <tejas@tejass-MacBook-Air.local>
2026-06-13 03:02:05 +0530

    Fix Vite base path for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Fly-beyond-the-Limits/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
